c33202d0ba10b6a34de4c9fda4ef23a23b4891d0
Hutatsuiwa <1241533720@qq.com>
2021-11-30 23:26:39 +0800

    完成了数据验证模块的编写，但尚未测试完成

--- test4.js
+++ test4.js
@@ -5,4 +5,31 @@ var a = setTimeout(() => {
 Number(a)
 
 
+setTimeout(()=>{
+    console.log("setTimeout1")
+    setTimeout(()=>{
+        console.log("setTimeout2")
+    },0)
+    Promise.resolve().then(()=>{
+        console.log("promise1")
+        Promise.resolve().then(()=>{
+            console.log("promise2")
+        })
+    })
+},0)
+
+setTimeout(()=>{
+    console.log("setTimeout1")
+    Promise.resolve().then(()=>{
+        console.log("promise1")
+    })
+},0)
+
+setTimeout(()=>{
+    console.log("setTimeout2")
+    Promise.resolve().then(()=>{
+        console.log("promise2")
+    })
+},0)
+
 // console.log(a)
